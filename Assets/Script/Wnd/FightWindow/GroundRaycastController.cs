609a0fa58c4a81c03525dafef087d8fb0ca51641
DESKTOP-N9U5QTI\max00 <max00227@gmail.com>
2019-01-01 17:19:17 +0800

    ChangeLog

--- Assets/Script/Wnd/FightWindow/GroundRaycastController.cs
+++ Assets/Script/Wnd/FightWindow/GroundRaycastController.cs
@@ -86,7 +86,7 @@ public class GroundRaycastController : MonoBehaviour {
 
 	private RaycastHit2D[] GetRaycastHits(Vector2 org, Vector2 dir, float dis) {
 		LayerMask mask = 1 << 8;
-        Debug.LogError(org.x + " : " + org.y + " , " + dir.x + " : " + dir.y);
+        Debug.LogError(org.x + " : " + org.y + " , " + dis);
 		RaycastHit2D[] hits = Physics2D.RaycastAll(org, dir, dis);
 
 		return hits;
